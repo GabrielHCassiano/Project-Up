9462588da4f2b600393076bf8b749eab19f395a9
Gabriel <gabriel.h.cassiano@gmail.com>
2024-08-29 16:16:07 -0300

    Set Inputs Config

---
+++ Assets/Scripts/Inputs/InputsPlayers.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputsPlayers : MonoBehaviour
+{
+    [SerializeField] private Vector2 moveDirection;
+    [SerializeField] private bool button1, button2, button3, button4;
+    [SerializeField] private bool buttonR1, buttonR2, buttonL1, buttonL2;
+    [SerializeField] private bool buttonStart, buttonSelect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region GetAndSet
+
+    public Vector2 MoveDirection 
+    { 
+        get { return moveDirection; } 
+        set {  moveDirection = value; }
+    }
+
+    public bool Button1
+    {
+        get { return button1; }
+        set { button1 = value; }
+    }
+
+    public bool Button2
+    {
+        get { return button2; }
+        set { button2 = value; }
+    }
+
+    public bool Button3
+    {
+        get { return button3; }
+        set { button3 = value; }
+    }
+
+    public bool Button4
+    {
+        get { return button4; }
+        set { button4 = value; }
+    }
+
+    public bool ButtonR1
+    {
+        get { return buttonR1; }
+        set { buttonR1 = value; }
+    }
+
+    public bool ButtonR2
+    {
+        get { return buttonR2; }
+        set { buttonR2 = value; }
+    }
+
+    public bool ButtonL1
+    {
+        get { return buttonL1; }
+        set { buttonL1 = value; }
+    }
+
+    public bool ButtonL2
+    {
+        get { return buttonL2; }
+        set { buttonL2 = value; }
+    }
+
+    public bool ButtonStart
+    {
+        get { return buttonStart; }
+        set { buttonStart = value; }
+    }
+
+    public bool ButtonSelect
+    {
+        get { return buttonSelect; }
+        set { buttonSelect = value; }
+    }
+
+    #endregion
+
+    #region ButtonsSet
+
+    public void MoveSet(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+
+    public void Button1Set(InputAction.CallbackContext context)
+    {
+        button1 = context.action.triggered;
+    }
+
+    public void Button2Set(InputAction.CallbackContext context)
+    {
+        button2 = context.action.triggered;
+    }
+
+    public void Button3Set(InputAction.CallbackContext context)
+    {
+        button3 = context.action.triggered;
+    }
+
+    public void Button4Set(InputAction.CallbackContext context)
+    {
+        button4 = context.action.triggered;
+    }
+
+    public void ButtonR1Set(InputAction.CallbackContext context)
+    {
+        buttonR1 = context.action.triggered;
+    }
+
+    public void ButtonR2Set(InputAction.CallbackContext context)
+    {
+        buttonR2 = context.action.triggered;
+    }
+
+    public void ButtonL1Set(InputAction.CallbackContext context)
+    {
+        buttonL1 = context.action.triggered;
+    }
+
+    public void ButtonL2Set(InputAction.CallbackContext context)
+    {
+        buttonL2 = context.action.triggered;
+    }
+
+    public void ButtonStartSet(InputAction.CallbackContext context)
+    {
+        buttonStart = context.action.triggered;
+    }
+
+    public void ButtonSelectSet(InputAction.CallbackContext context)
+    {
+        buttonSelect = context.action.triggered;
+    }
+
+    #endregion
+}
